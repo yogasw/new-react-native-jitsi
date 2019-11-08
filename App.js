e2b15e53a52247f21d77110eb9eed47bc54cd755
yoga setiawan <yogainformatika@gmail.com>
2019-11-08 15:48:54 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View } from 'react-native';
+import JitsiMeet, { JitsiMeetView } from 'react-native-jitsi-meet';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onConferenceTerminated = this.onConferenceTerminated.bind(this);
+    this.onConferenceJoined = this.onConferenceJoined.bind(this);
+    this.onConferenceWillJoin = this.onConferenceWillJoin.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      const url = "https://meet.jit.si/yoga"
+      JitsiMeet.call(url);
+      /* You can also use JitsiMeet.audioCall(url) for audio only call */
+      /* You can programmatically end the call with JitsiMeet.endCall() */
+    }, 1000);
+  }
+
+  onConferenceTerminated(nativeEvent) {
+    /* Conference terminated event */
+  }
+
+  onConferenceJoined(nativeEvent) {
+    /* Conference joined event */
+  }
+
+  onConferenceWillJoin(nativeEvent) {
+    /* Conference will join event */
+  }
+
+  render() {
+    return (
+        <View style={{ backgroundColor: 'black' }}>
+          <JitsiMeetView onConferenceTerminated={this.onConferenceTerminated} onConferenceJoined={this.onConferenceJoined} onConferenceWillJoin={this.onConferenceWillJoin} style={{ flex: 1, height: '100%', width: '100%' }} />
+        </View>
+    );
+  }
+}
+
+export default App;
